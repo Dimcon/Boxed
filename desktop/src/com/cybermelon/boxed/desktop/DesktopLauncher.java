c581c0275d4a6b2605038db7b00c5a5c23540d58
Daimon Sewell <Daimonsewell@gmail.com>
2015-12-02 16:24:40 +0200

    Initial commit. LibGDX project. LibGDX works with gradle so no more setup is required. Checkout this repo from Github, Create a new local branch and start coding!

---
+++ desktop/src/com/cybermelon/boxed/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cybermelon.boxed.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cybermelon.boxed.BoxedMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BoxedMain(), config);
+	}
+}
